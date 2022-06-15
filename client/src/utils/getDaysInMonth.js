ba46163ef051f79719bb7df9ffb590e27f26d788
tamminh2003 <tamminh2003@gmail.com>
2022-06-15 19:27:14 +1000

    Fix month ends sunday extend another week.

--- client/src/utils/getDaysInMonth.js
+++ client/src/utils/getDaysInMonth.js
@@ -9,7 +9,8 @@ export default function getDaysInMonth(date) {
   const monthEnd = _Date.lastDayOfMonth(monthStart);
   monthDays.push(monthStart);
   let day = _Date.addDays(monthStart, 1);
-  while ((_Date.compareAsc(day, monthEnd) !== 1 || _Date.getDay(day) !== 0)) {
+  while ((_Date.compareAsc(day, monthEnd) !== 1 || _Date.getDay(day) !== 0)
+    && (_Date.compareAsc(day, monthEnd) !== 0 || _Date.getDay(day) !== 0)) {
     monthDays.push(day);
     day = _Date.addDays(day, 1);
   }
