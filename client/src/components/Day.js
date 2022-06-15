3d5a7d1877c08a27be884a8e8d8715e4d711dc8d
tamminh2003 <tamminh2003@gmail.com>
2022-06-15 20:11:35 +1000

    Moved common className to external variable

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -22,22 +22,24 @@ export default function Day(props) {
     setShifts(await fetchShifts(date));
   }
 
+  const className = "flex h-10 items-center justify-center  border rounded-full font-semibold drop-shadow-lg active:scale-75 transform duration-75";
+
   return (
     <div className='h-10 w-10 day-container'>
 
       {shift && shift.shiftType === 'am'
-        && <div className="flex h-10 items-center justify-center  border rounded-full bg-teal-500 text-slate-200 font-semibold">
+        && <div className={`${className} bg-teal-500 text-slate-200`}>
           {_Date.getDate(day)}
         </div>}
       {shift && shift.shiftType === 'pm'
-        && <div className="flex h-10 items-center justify-center  border rounded-full bg-purple-500 text-white font-semibold">
+        && <div className={`${className} bg-purple-500 text-white`}>
           {_Date.getDate(day)}
         </div>}
       {shift && shift.shiftType === 'night'
-        && <div className="flex h-10 items-center justify-center  border rounded-full bg-indigo-600 text-white font-semibold">
+        && <div className={`${className} bg-indigo-600 text-white`}>
           {_Date.getDate(day)}
         </div>}
-      {!shift && <div className="flex h-10 items-center justify-center  border rounded-full bg-indigo-200 text-slate-600 font-semibold">
+      {!shift && <div className={`${className} bg-indigo-200 text-slate-600`}>
         {_Date.getDate(day)}
       </div>}
     </div>
