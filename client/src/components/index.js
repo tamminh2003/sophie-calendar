0acabe620c8c2775d941b7b27c5cad3d657ab089
tamminh2003 <tamminh2003@gmail.com>
2022-06-16 13:28:38 +1000

    Add MenuBar, ControlButtons Components.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 import Month from "./Month";
 import Day from "./Day";
+import MenuBar from "./MenuBar";
+import ControlButtons from "./ControlButtons";
 
-export { Month, Day };
+export { Month, Day, MenuBar, ControlButtons };
