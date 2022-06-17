eadb6ca1af7c16ec3f41651e6adeef5ea94b7b78
tamminh2003 <tamminh2003@gmail.com>
2022-06-17 12:44:37 +1000

    Added Middle Control button

--- client/src/components/ControlButtons.js
+++ client/src/components/ControlButtons.js
@@ -1,28 +1,49 @@
+import { useState } from 'react';
 import * as _Date from 'date-fns';
 import * as Comps from '.';
+import { Button, Dialog } from '@mui/material';
+import { getStyle } from '../utils';
 
 
-const ControlButtons = function ({ date, setDate }) {
+const ControlButtons = function ({ date, setDate, mode, setMode }) {
+  const [modeSelect, setModeSelect] = useState(false);
+  function handleCloseModeSelect(e) { setModeSelect(false); }
+  function handleOpenModeSelect(e) { setModeSelect(true); }
   function handlePrevClick(e) { setDate(_Date.addMonths(date, -1)); }
   function handleNextClick(e) { setDate(_Date.addMonths(date, 1)); }
 
-  const className = "select-none text-white mx-4 px-4 rounded-full border font-semibold transform duration-75 active:bg-opacity-50 scale-110 cursor-default";
+  const buttonStyle = {
+    other: "select-none cursor-default",
+    font: "text-white font-semibold",
+    size: "mx-4 px-4 h-7 scale-110",
+    border: "border rounded-full",
+    transform: "transform duration-75 active:bg-opacity-50"
+  }
 
   return (
     <div className="absolute bottom-0 w-screen">
-      <Comps.DatePicker date={date} setDate={setDate} />
-      <div className="flex justify-center my-4">
-        <div className={`${className} bg-teal-500`}>AM</div>
-        <div className={`${className} bg-purple-500`}>PM</div>
-        <div className={`${className} bg-indigo-600`}>Night</div>
-      </div>
-      <div className="flex justify-center my-4">
-        <div className={`${className} bg-sky-400`} onClick={handlePrevClick}>Prev</div>
-        <div className={`${className} bg-sky-400`} onClick={handleNextClick}>Next</div>
-      </div>
 
+      <Comps.DatePicker date={date} setDate={setDate} />
+
+      <div className="flex justify-between h-10 my-4">
+        <div className="my-auto">
+          <div className={`${getStyle(buttonStyle)} bg-sky-400 `} onClick={handlePrevClick}>Prev</div>
+        </div>
+        <div className="my-auto">
+          <div className={`${getStyle(buttonStyle)} bg-sky-400 relative`}><span className="h-2 my-auto select-none" onClick={handleOpenModeSelect}>+</span></div>
+        </div>
+        <Dialog open={modeSelect} onClose={handleCloseModeSelect}>
+          <Button onClick={() => { setMode('am'); handleCloseModeSelect(); }}>AM</Button>
+          <Button onClick={() => { setMode('pm'); handleCloseModeSelect(); }}>PM</Button>
+          <Button onClick={() => { setMode('night'); handleCloseModeSelect(); }}>Night</Button>
+          <Button onClick={() => { setMode('delete'); handleCloseModeSelect(); }}>Delete</Button>
+        </Dialog>
+        <div className="my-auto">
+          <div className={`${getStyle(buttonStyle)} bg-sky-400`} onClick={handleNextClick}>Next</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ControlButtons
