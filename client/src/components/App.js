6cd7b1b78532999859eb56da409976fa0a9237bf
tamminh2003 <tamminh2003@gmail.com>
2022-06-17 12:44:20 +1000

    Added react-swipeable

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import * as _Date from 'date-fns';
+import { useSwipeable } from 'react-swipeable';
 
 import * as Comps from '.';
 import { getStyle } from '../utils';
@@ -10,17 +11,20 @@ function App() {
   const [date, setDate] = useState(_Date.startOfMonth(now));
   const [mode, setMode] = useState(null);
 
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => setDate(_Date.addMonths(date, -1)),
+    onSwipedRight: () => setDate(_Date.addMonths(date, 1))
+  });
+
   const style = {
     background: "bg-gradient-to-b from-sky-400 to-indigo-500",
-    size: "w-screen h-screen"
+    size: "w-full h-screen"
   }
 
   return (
-    <div id="App" className={getStyle(style)}>
-      <Comps.MenuBar />
-      <Comps.Month date={date} setDate={setDate} />
-      
-      <Comps.ControlButtons date={date} setDate={setDate} />
+    <div id="App" className={getStyle(style)} {...swipeHandlers}>
+      <Comps.Month date={date} setDate={setDate} mode={mode} />
+      <Comps.ControlButtons date={date} setDate={setDate} mode={mode} setMode={setMode}/>
     </div>
   );
 }
